38773a1feacb7979c694f5dc3d4772dc684b2aff
Jan-Lukas-Richert-th-ab-ab <115979492+Jan-Lukas-Richert-th-ab-de@users.noreply.github.com>
2022-11-23 09:24:47 +0100

    Umändern der Core Resources Klassen und umschreiben der argumente. (Doctor,Patient,Room)

--- src/er/ER_doctor.h
+++ src/er/ER_doctor.h
@@ -1,23 +1,27 @@
-#ifndef ER_PERSON_H
-#define ER_PERSON_H
+#ifndef ER_Doctor_H
+#define ER_Doctor_H
 #include <string>
 
-class ER_Person {
+class ER_Doctor
+{
 public:
-    ER_Person() = delete;
-    ER_Person(std::string fn, std::string ln);
-    ER_Person(unsigned int id, std::string fn, std::string ln);
+    ER_Doctor() = delete;
+    ER_Doctor(std::string fn, std::string ln);
+    ER_Doctor(unsigned int id, std::string fn, std::string ln, std::string dc);
     unsigned int get_id() const;
     std::string get_first_name() const;
     std::string get_last_name() const;
+    std::string get_doctors_specialties() const;
     std::string get_full_name() const;
-    void set_first_name(std::string fn); 
-    void set_last_name(std::string ln); 
+    void set_first_name(std::string fn);
+    void set_last_name(std::string ln);
+    void set_doctors_specialties(std::string ds);
 
 private:
     unsigned int id{0};
     std::string first_name{};
     std::string last_name{};
+    std::string doctors_specialties{};
 };
 
-#endif /* ER_PERSON_H */
+#endif /* ER_Doctor_H */
--- src/er/ER_patient.cpp
+++ src/er/ER_patient.cpp
@@ -1,32 +1,38 @@
-#include "ER_Person.h"
+#include "ER_Patient.h"
 
-ER_Person::ER_Person(std::string fn, std::string ln)
-   : first_name{fn}, last_name{ln} {};
+ER_Patient::ER_Patient(std::string fn, std::string ln)
+	: first_name{fn}, last_name{ln} {};
 
-ER_Person::ER_Person(unsigned int id, std::string fn, std::string ln)
-   : id{id}, first_name{fn}, last_name{ln} {};
+ER_Patient::ER_Patient(unsigned int id, std::string fn, std::string ln)
+	: id{id}, first_name{fn}, last_name{ln} {};
 
-unsigned int ER_Person::get_id() const {
+unsigned int ER_Patient::get_id() const
+{
 	return id;
 };
 
-std::string ER_Person::get_first_name() const {
+std::string ER_Patient::get_first_name() const
+{
 	return first_name;
 };
-std::string ER_Person::get_last_name() const {
+std::string ER_Patient::get_last_name() const
+{
 	return last_name;
 };
-void ER_Person::set_first_name(std::string fn) {
-    first_name = fn;
-}; 
-void ER_Person::set_last_name(std::string ln) {
-    last_name = ln;
-}; 
+void ER_Patient::set_first_name(std::string fn)
+{
+	first_name = fn;
+};
+void ER_Patient::set_last_name(std::string ln)
+{
+	last_name = ln;
+};
 
-std::string ER_Person::get_full_name() const {
+std::string ER_Patient::get_full_name() const
+{
 	std::string result{""};
 	result.append(first_name);
 	result.append(" ");
 	result.append(last_name);
-    return result;
+	return result;
 }
