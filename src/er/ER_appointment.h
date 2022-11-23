34e928c10bb7b6b4d7837ad57e1ddbc1354b2305
Jan-Lukas-Richert-th-ab-ab <115979492+Jan-Lukas-Richert-th-ab-de@users.noreply.github.com>
2022-11-23 09:28:44 +0100

    Fix des #ifndef Argumetes

--- src/er/ER_appointment.h
+++ src/er/ER_appointment.h
@@ -1,28 +1,27 @@
-#iddef ER_APPOINTMENT_H
+#ifndef ER_APPOINTMENT_H
 #define ER_APPOINTMENT_H
 #include <string>
 
-class ER_Appointment {
+class ER_Appointment
+{
 public:
     ER_Appointment() = delete;
-    ER_Appointment(std::string d, std::string m, std::string y); //d, m, y
+    ER_Appointment(std::string d, std::string m, std::string y);                  // d, m, y
     ER_Appointment(unsigned int id, std::string d, std::string m, std::string y); // d, m, y
     unsigned int get_id() const;
-    std::string get_day() const; //Tag
-    std::string get_month() const;//Monat
-    std::string get_year() const; // Jahr noch dazu eingefügt
-    std::string get_date() const; //Datum
-    void set_day(std::string d); //Tag
-    void set_month(std::string m); //Monat
-    void set_year(std::string y); // Jahr noch dazu 
-
+    std::string get_day() const;   // Tag
+    std::string get_month() const; // Monat
+    std::string get_year() const;  // Jahr noch dazu eingefügt
+    std::string get_date() const;  // Datum
+    void set_day(std::string d);   // Tag
+    void set_month(std::string m); // Monat
+    void set_year(std::string y);  // Jahr noch dazu
 
 private:
     unsigned int id{0};
-    std::string day{};//Tag
-    std::string month{};//Monat
-    std::string year{}; //Jahr noch dazu
-
+    std::string day{};   // Tag
+    std::string month{}; // Monat
+    std::string year{};  // Jahr noch dazu
 };
 
 #endif /* ER_Appointment_H */
