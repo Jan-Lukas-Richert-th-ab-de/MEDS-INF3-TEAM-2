6fc1ea2e393948e09a9e66b46c0d170c9455eee0
Anas-Karah-th-ab-de <s210835@th-ab.de>
2022-11-30 23:43:44 +0100

    Angepasst

---
+++ src/ar/AR_Summary_Statistics_View.cpp
@@ -0,0 +1,222 @@
+AR_Summary_Statistics_View:CPP
+//  Created by Anas Karah on 30/11/22
+
+#include "AR_Summary_Statistics_View.h"
+
+AR_Summary_Statistics_View_Doctor::AR_Summary_Statistics_View_Doctor()
+{
+    
+}
+
+
+
+
+void AR_Summary_Statistics_View_Doctor::setDoctorName(string DoctorName)
+{
+    this->DoctorName = DoctorName;
+}
+
+
+
+void AR_Summary_Statistics_View_Doctor::setDoctorID(string DoctorID)
+{
+    this->DoctorID = DoctorID;
+}
+
+
+
+void AR_Summary_Statistics_View_Doctor::setDoctorWorkingDays(string DoctorWorkingDays)
+{
+    this->DoctorWorkingDays = DoctorWorkingDays;
+}
+
+
+
+void AR_Summary_Statistics_View_Doctor::setDoctorWorkingYears(string DoctorWorkingYears)
+{
+    this->DoctorWorkingYears = DoctorWorkingYears;
+}
+
+
+
+void AR_Summary_Statistics_View_Doctor::setDoctorNumberOfPatients(string DoctorNumberOfPatients)
+{
+    this->DoctorNumberOfPatients = DoctorNumberOfPatients;
+}
+
+
+
+void AR_Summary_Statistics_View_Doctor::setDoctorNumberOfAppointments(string DoctorNumberOfAppointments)
+{
+    this->DoctorNumberOfAppointments = DoctorNumberOfAppointments;
+}
+
+
+
+
+
+
+void AR_Summary_Statistics_View_Doctor::setDoctorNumberOfPatientsPerDay(string DoctorNumberOfPatientsPerDay)
+{
+    this->DoctorNumberOfPatientsPerDay = DoctorNumberOfPatientsPerDay;
+}
+void AR_Summary_Statistics_View_Doctor::setDoctorNumberOfAppointmentsPerDay(string DoctorNumberOfAppointmentsPerDay)
+{
+    this->DoctorNumberOfAppointmentsPerDay = DoctorNumberOfAppointmentsPerDay;
+}
+
+
+
+
+void AR_Summary_Statistics_View_Doctor::setDoctorNumberOfPatientsPerMonth(string DoctorNumberOfPatientsPerMonth)
+{
+    this->DoctorNumberOfPatientsPerMonth = DoctorNumberOfPatientsPerMonth;
+}
+void AR_Summary_Statistics_View_Doctor::setDoctorNumberOfAppointmentsPerMonth(string DoctorNumberOfAppointmentsPerMonth)
+{
+    this->DoctorNumberOfAppointmentsPerMonth = DoctorNumberOfAppointmentsPerMonth;
+}
+
+
+
+void AR_Summary_Statistics_View_Doctor::setDoctorNumberOfPatientsPerYear(string DoctorNumberOfPatientsPerYear)
+{
+    this->DoctorNumberOfPatientsPerYear = DoctorNumberOfPatientsPerYear;
+}
+void AR_Summary_Statistics_View_Doctor::setDoctorNumberOfAppointmentsPerYear(string DoctorNumberOfAppointmentsPerYear)
+{
+    this->DoctorNumberOfAppointmentsPerYear = DoctorNumberOfAppointmentsPerYear;
+}
+
+
+
+void AR_Summary_Statistics_View_Doctor::setDoctorNumberOfPatientsPerWeek(string DoctorNumberOfPatientsPerWeek)
+{
+    this->DoctorNumberOfPatientsPerWeek = DoctorNumberOfPatientsPerWeek;
+}
+void AR_Summary_Statistics_View_Doctor::setDoctorNumberOfAppointmentsPerWeek(string DoctorNumberOfAppointmentsPerWeek)
+{
+    this->DoctorNumberOfAppointmentsPerWeek = DoctorNumberOfAppointmentsPerWeek;
+}
+
+
+string AR_Summary_Statistics_View_Doctor::getDoctorName()
+{
+    return DoctorName;
+}
+
+
+string AR_Summary_Statistics_View_Doctor::getDoctorID()
+{
+    return DoctorID;
+}
+
+
+string AR_Summary_Statistics_View_Doctor::getDoctorWorkingDays()
+{
+    return DoctorWorkingDays;
+}
+
+
+string AR_Summary_Statistics_View_Doctor::getDoctorWorkingYears()
+{
+    return DoctorWorkingYears;
+}
+
+
+string AR_Summary_Statistics_View_Doctor::getDoctorNumberOfPatients()
+{
+    return DoctorNumberOfPatients;
+}
+
+
+string AR_Summary_Statistics_View_Doctor::getDoctorNumberOfAppointments()
+{
+    return DoctorNumberOfAppointments;
+}
+
+
+
+
+string AR_Summary_Statistics_View_Doctor::getDoctorNumberOfPatientsPerDay()
+{
+    return DoctorNumberOfPatientsPerDay;
+}
+string AR_Summary_Statistics_View_Doctor::getDoctorNumberOfAppointmentsPerDay()
+{
+    return DoctorNumberOfAppointmentsPerDay;
+}
+
+
+
+string AR_Summary_Statistics_View_Doctor::getDoctorNumberOfPatientsPerMonth()
+{
+    return DoctorNumberOfPatientsPerMonth;
+}
+string AR_Summary_Statistics_View_Doctor::getDoctorNumberOfAppointmentsPerMonth()
+{
+    return DoctorNumberOfAppointmentsPerMonth;
+}
+
+
+
+
+string AR_Summary_Statistics_View_Doctor::getDoctorNumberOfPatientsPerYear()
+{
+    return DoctorNumberOfPatientsPerYear;
+}
+
+string AR_Summary_Statistics_View_Doctor::getDoctorNumberOfAppointmentsPerYear()
+{
+    return DoctorNumberOfAppointmentsPerYear;
+}
+
+
+
+string AR_Summary_Statistics_View_Doctor::getDoctorNumberOfPatientsPerWeek()
+{
+    return DoctorNumberOfPatientsPerWeek;
+}
+string AR_Summary_Statistics_View_Doctor::getDoctorNumberOfAppointmentsPerWeek()
+{
+    return DoctorNumberOfAppointmentsPerWeek;
+}
+
+//ROOM
+
+void AR_Summary_Statistics_View_Room::setRoomName(string RoomName)
+{
+    this->RoomName = RoomName;
+}
+
+void AR_Summary_Statistics_View_Room::setRoomID(string RoomID)
+{
+    this->RoomID = RoomID;
+}
+
+
+
+
+
+}
+
+string AR_Summary_Statistics_View_Room::getRoomName()
+{
+    return RoomName;
+}
+
+string AR_Summary_Statistics_View_Room::getRoomID()
+{
+    return RoomID;
+}
+
+
+}
+
+
+
+
+
+
+
+
