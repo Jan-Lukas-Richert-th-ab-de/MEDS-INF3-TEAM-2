004e961d46ea6a01bcc420880e8d9ac8ffb29ed8
Jan-Lukas-Richert-th-ab <115979492+Jan-Lukas-Richert-th-ab-de@users.noreply.github.com>
2022-12-05 09:57:02 +0100

    Befehl für Anas eingefügt

--- src/ar/AR_Summary_Statistics_View.h
+++ src/ar/AR_Summary_Statistics_View.h
@@ -1,5 +1,5 @@
-//AR_Summary_Statistics_View_h
-//  Created by Anas Karah on 30/11/22
+// AR_Summary_Statistics_View_h
+//   Created by Anas Karah on 30/11/22
 
 #ifndef AR_SUMMARY_STATISTICS_VIEW_H
 #define AR_SUMMARY_STATISTICS_VIEW_H
@@ -16,165 +16,146 @@ class AR_Summary_Statistics_View_Doctor
 
 {
 public:
-    AR_Summary_Statistics_View_Doctor();
-    ~AR_Summary_Statistics_View_Doctor();
-    
-    void setDoctorName(string);
-    void setDoctorID(string);
-    
-    void setDoctorWorkingDays(string);
-    void setDoctorWorkingYears(string);
-    
-    void setDoctorNumberOfPatients(string);
-    void setDoctorNumberOfAppointments(string);
-    
-    void setDoctorNumberOfPatientsPerDay(string);
-    void setDoctorNumberOfAppointmentsPerDay(string);
-   
-    void setDoctorNumberOfPatientsPerMonth(string);
-    void setDoctorNumberOfAppointmentsPerMonth(string);
-   
-    void setDoctorNumberOfPatientsPerYear(string);
-    void setDoctorNumberOfAppointmentsPerYear(string);
-    
-    void setDoctorNumberOfPatientsPerWeek(string);
-    void setDoctorNumberOfAppointmentsPerWeek(string);
+   AR_Summary_Statistics_View_Doctor();
+   ~AR_Summary_Statistics_View_Doctor();
 
-      string getDoctorName();
-      string getDoctorID();
-      
-      string getDoctorWorkingHours();
-      string getDoctorWorkingDays();
-      
-      string getDoctorWorkingYears();
-     
-      string getDoctorNumberOfPatients();
-      string getDoctorNumberOfAppointments();
-      
-      
-   
-    
-      string getDoctorNumberOfAppointmentsPerDay();
-      string getDoctorNumberOfPatientsPerMonth();
+   void setDoctorName(string);
+   void setDoctorID(string);
+
+   void setDoctorWorkingDays(string);
+   void setDoctorWorkingYears(string);
+
+   void setDoctorNumberOfPatients(string);
+   void setDoctorNumberOfAppointments(string);
+
+   void setDoctorNumberOfPatientsPerDay(string);
+   void setDoctorNumberOfAppointmentsPerDay(string);
+
+   void setDoctorNumberOfPatientsPerMonth(string);
+   void setDoctorNumberOfAppointmentsPerMonth(string);
+
+   void setDoctorNumberOfPatientsPerYear(string);
+   void setDoctorNumberOfAppointmentsPerYear(string);
+
+   void setDoctorNumberOfPatientsPerWeek(string);
+   void setDoctorNumberOfAppointmentsPerWeek(string);
+
+   string getDoctorName();
+   string getDoctorID();
+
+   string getDoctorWorkingHours();
+   string getDoctorWorkingDays();
+
+   string getDoctorWorkingYears();
+
+   string getDoctorNumberOfPatients();
+   string getDoctorNumberOfAppointments();
+
+   string getDoctorNumberOfAppointmentsPerDay();
+   string getDoctorNumberOfPatientsPerMonth();
    ;
-     
-      
-      string getDoctorNumberOfAppointmentsPerMonth();
-      string getDoctorNumberOfPatientsPerYear();
-     
-   
-      string getDoctorNumberOfAppointmentsPerYear();
-      string getDoctorNumberOfPatientsPerWeek();
-     
-     
-      string getDoctorNumberOfPrescriptionsPerWeek();
-      string getDoctorNumberOfAppointmentsPerWeek();
+
+   string getDoctorNumberOfAppointmentsPerMonth();
+   string getDoctorNumberOfPatientsPerYear();
+
+   string getDoctorNumberOfAppointmentsPerYear();
+   string getDoctorNumberOfPatientsPerWeek();
+
+   string getDoctorNumberOfPrescriptionsPerWeek();
+   string getDoctorNumberOfAppointmentsPerWeek();
 
 private:
-      string DoctorName;
-      string DoctorID;
-     
-      string DoctorWorkingHours;
-      string DoctorWorkingDays;
-      string DoctorWorkingYears;
-      
-      string DoctorNumberOfPatients;
-      string DoctorNumberOfAppointments;
-     
-      string DoctorNumberOfPatientsPerDay; 
-      string DoctorNumberOfAppointmentsPerDay;
-       
-      string DoctorNumberOfPatientsPerWeek;
-      string DoctorNumberOfAppointmentsPerWeek;
+   string DoctorName;
+   string DoctorID;
 
-      string DoctorNumberOfPatientsPerMonth;
-      string DoctorNumberOfAppointmentsPerMonth;
-      
-      string DoctorNumberOfPatientsPerYear;
-      string DoctorNumberOfAppointmentsPerYear;
-      
-     
-   };
+   string DoctorWorkingHours;
+   string DoctorWorkingDays;
+   string DoctorWorkingYears;
 
+   string DoctorNumberOfPatients;
+   string DoctorNumberOfAppointments;
+
+   string DoctorNumberOfPatientsPerDay;
+   string DoctorNumberOfAppointmentsPerDay;
+
+   string DoctorNumberOfPatientsPerWeek;
+   string DoctorNumberOfAppointmentsPerWeek;
+
+   string DoctorNumberOfPatientsPerMonth;
+   string DoctorNumberOfAppointmentsPerMonth;
+
+   string DoctorNumberOfPatientsPerYear;
+   string DoctorNumberOfAppointmentsPerYear;
+};
 
 class AR_Summary_Statistics_View_Room
 {
 public:
-    AR_Summary_Statistics_View_Room();
-    ~AR_Summary_Statistics_View_Room();
-   
-    void setRoomName(string);
-    void setRoomID(string);
-    void setRoomType(string);
+   AR_Summary_Statistics_View_Room();
+   ~AR_Summary_Statistics_View_Room();
 
-      
-  
-    void setRoom(string);
-   
-    void setRoomNumberOfAppointments(string);
-  
-    void setRoomPerDay(string);
-   
-    void setRoomNumberOfAppointmentsPerDay(string);
+   void setRoomName(string);
+   void setRoomID(string);
+   void setRoomType(string);
 
-    void setRoomPerMonth(string);
-  
-    void setRoomNumberOfAppointmentsPerMonth(string);
+   void setRoom(string);
 
-    void setRoomPerYear(string);
-  
-    void setRoomNumberOfAppointmentsPerYear(string);
- ;
-    void setRoomPerWeek(string);
-   
-    void setRoomNumberOfAppointmentsPerWeek(string);
+   void setRoomNumberOfAppointments(string);
 
-    string getRoomName();
-    string getRoomID();
-    string getRoomType();
+   void setRoomPerDay(string);
 
-    string getRoom();
-   
-    
-    string getRoomNumberOfAppointments();
- 
+   void setRoomNumberOfAppointmentsPerDay(string);
 
-   
-   
-    string getRoomNumberOfAppointmentsPerDay();
+   void setRoomPerMonth(string);
 
+   void setRoomNumberOfAppointmentsPerMonth(string);
 
-  
-    string getRoomNumberOfAppointmentsPerMonth();
+   void setRoomPerYear(string);
 
-    
-    string getRoomNumberOfAppointmentsPerYear();
+   void setRoomNumberOfAppointmentsPerYear(string);
+   ;
+   void setRoomPerWeek(string);
 
-   
-    
-      string getRoomNumberOfAppointmentsPerWeek();
+   void setRoomNumberOfAppointmentsPerWeek(string);
+
+   string getRoomName();
+   string getRoomID();
+   string getRoomType();
+
+   string getRoom();
+
+   string getRoomNumberOfAppointments();
+
+   string getRoomNumberOfAppointmentsPerDay();
+
+   string getRoomNumberOfAppointmentsPerMonth();
+
+   string getRoomNumberOfAppointmentsPerYear();
+
+   string getRoomNumberOfAppointmentsPerWeek();
 
 private:
-      string RoomName;
-      string RoomID;
-      string RoomType;
-   
+   string RoomName;
+   string RoomID;
+   string RoomType;
 
+   string RoomNumberOfAppointmentsPerDay;
 
+   string RoomNumberOfAppointmentsPerWeek;
 
- 
-      string RoomNumberOfAppointmentsPerDay;
+   string RoomNumberOfAppointmentsPerMonth;
 
- 
-      string RoomNumberOfAppointmentsPerWeek;
-
-      string RoomNumberOfAppointmentsPerMonth;
-    
-
-      string RoomNumberOfAppointmentsPerYear;
-
-
-   };
-      
+   string RoomNumberOfAppointmentsPerYear;
+};
 
 #endif /* AR_Summary_Statistics_View_h */
+       /*std::string FD_DA_Patient_Repository_FileBasedImpl::create_storage_record_from_patient(unsigned int id, const ER_Patient &patient)
+       {
+         std::string result{};
+         result.append(std::to_string(id));
+         result.append(",");
+         result.append(patient.get_first_name());
+         result.append(",");
+         result.append(patient.get_last_name());
+         return result;
+       };*/
