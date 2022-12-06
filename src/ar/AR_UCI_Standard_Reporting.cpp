4654c8036341920490049e290b04c5d5e9f30185
rina-ismaili-th-ab-de <115979796+rina-ismaili-th-ab-de@users.noreply.github.com>
2022-12-06 20:00:49 +0100

    AR verbessert: Room and Doctor aus repository lade

--- src/ar/AR_UCI_Standard_Reporting.cpp
+++ src/ar/AR_UCI_Standard_Reporting.cpp
@@ -3,57 +3,39 @@
 #include <map>
 #include <string>
 #include <algorithm>
-#include "ER_Person.h"
+#include "ER_doctor.h"
+#include "ER_Room.h"
+#include "ER_appointment.h"
+
 
 AR_UCI_Standard_Reporting::AR_UCI_Standard_Reporting(
-    AR_UCI_Standard_Reporting_OB& presenter
-  , AR_DAI_Person_Repository& repository)
-            : resource_presenter(presenter)
-            , resource_repository(repository) {};
+    AR_UCI_Standard_Reporting_OB &presenter
+  , AR_DAI_Doctor_Repository& repositoryDoctor
+  , AR_DAI_Room_Repository& repositoryRoom)
+    : resource_presenter(presenter)
+    , resource_repository(repositoryDoctor)
+    , resource_repository(repositorRoom) {};
 
-void AR_UCI_Standard_Reporting::list_all(){
+void AR_UCI_Standard_Reporting::list_all()
+{
   std::vector<std::map<std::string, std::string>> output_data;
-  std::vector<ER_Person> all_persons = resource_repository.find_all();
-  sort_persons_by_id(all_persons);
-  for(auto current_person : all_persons) {
-    std::map<std::string, std::string> data_map_for_current_person = 
-        get_data_map_for_person(current_person);
-    output_data.push_back(data_map_for_current_person);
+  std::vector<ER_Doctor> all_doctors = resource_repository.find_all();
+  
+  sort_doctors_by_id(all_doctors);
+  for (auto current_doctor : all_doctors)
+  {
+    std::map<std::string, std::string> data_map_for_current_doctor =
+        get_data_map_for_doctor(current_doctor);
+    output_data.push_back(data_map_for_current_doctor);
   }
+
+  sort_rooms_by_id(all_room);
+  for (auto current_room: all_room)
+  {
+    std::map<std::string, std::string> data_map_for_current_room =
+        get_data_map_for_tour(current_room);
+    output_data.push_back(data_map_for_current_room);
+  }
+
   resource_presenter.present_all(output_data);
-};
-void AR_UCI_Standard_Reporting::create(std::string first_name, std::string last_name){
-  ER_Person a_person{first_name, last_name};
-  resource_repository.save(a_person);
-  list_all();
-};
-void AR_UCI_Standard_Reporting::update(unsigned int id, std::string first_name, std::string last_name){
-  ER_Person a_person_to_be_updated = resource_repository.find(id);
-  a_person_to_be_updated.set_first_name(first_name);
-  a_person_to_be_updated.set_last_name(last_name);
-  resource_repository.save(a_person_to_be_updated);
-  list_all();
-};
-void AR_UCI_Standard_Reporting::remove(unsigned int id){
-  resource_repository.remove(id);
-  list_all();
-};
-
-std::map<std::string, std::string> AR_UCI_Standard_Reporting::get_data_map_for_person(const ER_Person& a_person){
-    std::map<std::string, std::string> data_map{};
-    data_map.insert({"id",std::to_string(a_person.get_id())});
-    data_map.insert({"first_name",a_person.get_first_name()});
-    data_map.insert({"last_name",a_person.get_last_name()});
-    return data_map;
-};
-
-void AR_UCI_Standard_Reporting::sort_persons_by_id (std::vector<ER_Person>& person_list) {
-    std::sort(person_list.begin(), person_list.end(),
-    		[](ER_Person a, ER_Person b) {
-		   	auto result_a{a.get_id()};
-		  	auto result_b{b.get_id()};
-		  	return (result_a < result_b);
-		  	// the smaller result will be pushed to the beginning
-		    }
-	  );
 };
