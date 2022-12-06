b629dd9a26385f9ca3e208fbfc94942591ddaf68
rina-ismaili-th-ab-de <115979796+rina-ismaili-th-ab-de@users.noreply.github.com>
2022-12-06 18:45:35 +0100

    DAI_Standard Reporting gelöscht und paar andere Änderungen

--- src/ia/IA_Standard_Reporting_Controller.cpp
+++ src/ia/IA_Standard_Reporting_Controller.cpp
@@ -1,8 +1,8 @@
-#include "IA_Standard_Reportin_Controller.h"
+#include "IA_Standard_Reporting_Controller.h"
 #include <iostream>
 #include <string>
 
-IA_Standard_Reporting_Controller::IA_Standard_Reporting_Controller(AR_UCI_Person_Administration_IB& uci)
+IA_Standard_Reporting_Controller::IA_Standard_Reporting_Controller(AR_UCI_Standard_Reporting_IB& uci)
             : resource_uci(uci) {};
 void IA_Standard_Reporting_Controller::control_list_all() {
     resource_uci.list_all();
