25371c48b80664eece5ece088a21f6a9a2b59a1c
rina-ismaili-th-ab-de <115979796+rina-ismaili-th-ab-de@users.noreply.github.com>
2022-12-20 20:38:36 +0100

    IA entwickelt

--- src/ia/IA_Standard_Reporting_Controller.cpp
+++ src/ia/IA_Standard_Reporting_Controller.cpp
@@ -2,8 +2,28 @@
 #include <iostream>
 #include <string>
 
-IA_Standard_Reporting_Controller::IA_Standard_Reporting_Controller(AR_UCI_Standard_Reporting_IB& uci)
-            : resource_uci(uci) {};
-void IA_Standard_Reporting_Controller::control_list_all() {
-    resource_uci.list_all();
+
+
+IA_Standard_Reporting_Controller::IA_Standard_Reporting_Controller(AR_UCI_Standard_Reporting_IB& room_uci, AR_UCI_Standard_Reporting_IB& doctor_uci, AR_UCI_Standard_Reporting_IB& appointment_uci)
+            : report_room_uci(room_uci), report_doctor_uci (doctor_uci), report_appointment_uci (appointment_uci) {};
+
+
+
+void IA_Standard_Reporting_Controller::control_report_room() {
+    report_room_uci.report_room();
 };
+
+
+
+
+void IA_Standard_Reporting_Controller::control_report_doctor() {
+    report_doctor_uci.report_doctor();
+
+}
+
+
+
+void IA_Standard_Reporting_Controller::control_report_appointment() {
+    report_appointment_uci.report_appointment();
+
+}
--- src/ia/IA_Standard_Reporting_Presenter.cpp
+++ src/ia/IA_Standard_Reporting_Presenter.cpp
@@ -1,7 +1,23 @@
 #include "IA_Standard_Reporting_Presenter.h"
 
+
 IA_Standard_Reporting_Presenter::IA_Standard_Reporting_Presenter(FD_Resource_View& view)
             : resource_view(view) {};
 void IA_Standard_Reporting_Presenter::present_all(const std::vector<std::map<std::string, std::string>>& data) {
     resource_view.show_list_view(data);
+};
+
+void IA_Standard_Reporting_Presenter::present_report_room(size_t room) {
+   resource_view.show_view_report_room(room);
+
+};
+
+void IA_Standard_Reporting_Presenter::present_report_doctor(size_t doctor) {
+   resource_view.show_view_report_doctor(doctor);
+
+};
+
+void IA_Standard_Reporting_Presenter::present_report_appointment(size_t appointment) {
+   resource_view.show_view_report_appointment(appointment);
+
 };
