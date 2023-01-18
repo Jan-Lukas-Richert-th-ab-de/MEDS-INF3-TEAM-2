f2d39dfb2e0f46281a49725f32ef18c394eeecb1
Anas-Karah-th-ab-de <s210835@th-ab.de>
2023-01-19 00:12:19 +0100

    angepasst

--- src/fd/FD_Resource_View.h
+++ src/fd/FD_Resource_View.h
@@ -21,6 +21,12 @@ class FD_Resource_View_S : public FD_View_S
 
 public:
       virtual void show_list_view(const std::vector<std::map<std::string, std::string>> &data) = 0;
+            virtual void show_room_occupancy_summary(const std::vector<std::map<std::string, std::string>> &data)=0;
+            virtual void show_room_occupancy_summary_per_week(const std::vector<std::map<std::string, std::string>> &data)=0;
+
+            virtual void show_doctor_summary(const std::vector<std::map<std::string, std::string>> &data)=0;
+            virtual void show_doctor_summary_per_week(const std::vector<std::map<std::string, std::string>> &data)=0;
+
 
       virtual ~FD_Resource_View_S() = default;
       FD_Resource_View_S() = default;
